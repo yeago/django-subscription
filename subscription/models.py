e4bb7d74f2071125606e2e9cbbb545370b87ef00
yeago <subsume@gmail.com>
2011-08-20 22:46:44 -0400

    complete overhaul.

--- subscription/models.py
+++ subscription/models.py
@@ -1,17 +1,26 @@
 import datetime
+
 from django.db import models
-
-from django.core.mail import send_mail
 from django.conf import settings
-from django.template import loader, RequestContext
 
-from django.contrib import comments
-from django.contrib.comments.signals import comment_was_posted
-from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
 from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 
-CommentModel = comments.get_model()
+import subscription
+
+class SubscriptionManager(models.Manager):
+    def subscribe(self,user,obj):
+        ct = ContentType.objects.get_for_model(obj)
+        Subscription.objects.get_or_create(content_type=ct,object_id=obj.pk,user=user)
+
+    def emit(self,*args,**kwargs):
+        backend = kwargs.pop('backend',None) or None
+
+        if backend:
+            return subscription.get_backends()[backend]
+
+        for backend_module in subscription.get_backends().values():
+            backend_module(*args,**kwargs)
 
 class Subscription(models.Model):
     user = models.ForeignKey('auth.User')
@@ -19,8 +28,27 @@ class Subscription(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
     timestamp = models.DateTimeField(editable=False,default=datetime.datetime.now)
+    objects = SubscriptionManager()
     class Meta:
-	    db_table ="subscription"
+        db_table ="subscription"
+
+"""
+Seems sensible to auto-subscribe people to objects they comment on.
+
+Just send this with the comment_was_posted signal
+
+def auto_subscribe(**kwargs):
+    comment = kwargs.pop('comment')
+    auto_subscribe_field = getattr(settings,'SUBSCRIPTION_AUTOSUBSCRIBE_PROFILE_FIELD','auto_subscribe')
+    if getattr(comment.user.get_profile(),auto_subscribe_field,True):
+        Subscription.objects.subscribe(user,comment.content_object)
+
+comment_was_posted.connect(auto_subscribe, sender=CommentModel)
+"""
+
+"""
+Make abstract, turn into emit backend
+#comment_was_posted.connect(email_comment, sender=CommentModel)
 
 def email_comment(**kwargs):
     comment = kwargs.pop('comment')
@@ -35,15 +63,6 @@ def email_comment(**kwargs):
             object_id=comment.object_pk).exclude(user=comment.user)
 
     for i in subscriptions:
-	try:
-            i.user
-        except User.DoesNotExist:
-	    """
-	    User was deleted.
-	    """
-	    i.delete()
-	    continue
-
         if getattr(i.user.get_profile(),supress_email_field,False):
             continue
 
@@ -54,17 +73,9 @@ def email_comment(**kwargs):
             'delete': i.user.has_perm('comments.delete_comment'),
             'subscription': i,
         }
-	if not i.user.email:
+        if not i.user.email:
             continue
 
         send_mail(("%s - Comment on %s") % (site.name,comment.content_object), \
-       	    t.render(RequestContext(request,c)), None, [i.user.email])
-
-def auto_subscribe(**kwargs):
-    comment = kwargs.pop('comment')
-    auto_subscribe_field = getattr(settings,'SUBSCRIPTION_AUTOSUBSCRIBE_PROFILE_FIELD','auto_subscribe')
-    if getattr(comment.user.get_profile(),auto_subscribe_field,True):
-        Subscription.objects.get_or_create(user=comment.user,content_type=comment.content_type,object_id=comment.object_pk)
-
-comment_was_posted.connect(email_comment, sender=CommentModel)
-comment_was_posted.connect(auto_subscribe, sender=CommentModel)
+               t.render(RequestContext(request,c)), None, [i.user.email])
+"""
