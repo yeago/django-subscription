bc5de08261dad9b0b4836047a0fe384d72b1f527
yeago <subsume@gmail.com>
2011-08-20 23:19:08 -0400

    fix to backends to allow for one-off emissions

--- subscription/backends.py
+++ subscription/backends.py
@@ -9,9 +9,9 @@ class BaseBackend(object):
     def __call__(obj,*args,**kwargs):
         return obj(*args,**kwargs)
 
-    def __init__(self,obj,text,dont_send_to=None,send_only_to=None,actor=None,\
+    def __init__(self,text,subscribers_of=None,dont_send_to=None,send_only_to=None,actor=None,\
         actor_display_other=None,actor_display_self=None,format_kwargs=None,**kwargs):
-        # obj - Thing people are subscribed to
+        # subscribers_of - Thing people are subscribed to
         # dont_send_to / send_only_to - useful maybe?
         # text - string you want to emit.
         # actor_display_other - How does the actor appear to others? <a href="">{{ user.username }}</a>
@@ -21,13 +21,21 @@ class BaseBackend(object):
 
         # CAREFUL: If you send a typo-kwarg it will just be sent to emit(), so no error will raise =(
 
-        self.content_type = ContentType.objects.get_for_model(obj)
         self.kwargs = kwargs
 
         explicit_format_options = [i[1] for i in Formatter().parse(text)]
-        subscription_kwargs = {'content_type': self.content_type, 'object_id': obj.pk}
+
+        if not subscribers_of:
+            for recipient in send_only_to:
+                self.emit(recipient,text,**kwargs)
+
+            return
+
+        self.content_type = ContentType.objects.get_for_model(subscribers_of)
+        subscription_kwargs = {'content_type': self.content_type, 'object_id': subscribers_of.pk}
         if send_only_to:
             subscription_kwargs.update({'user__in': send_only_to})
+
         for i in Subscription.objects.filter(**subscription_kwargs):
             if i.user in (dont_send_to or []):
                 continue
