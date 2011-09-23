64952ac3c9a7c985050825fc974d493d3be977c7
James Pic <jamespic@gmail.com>
2011-09-23 08:21:17 +0200

    Bugfixes in app integration stuff and better actstream support

--- subscription/examples/yourlabs/models.py
+++ subscription/examples/yourlabs/models.py
@@ -10,12 +10,12 @@ if 'django.contrib.comments' in APPS:
     from django.contrib.comments.models import Comment
     from django.contrib.comments.signals import comment_was_posted
 
-    def auto_subscribe_user_to_his_comments(sender, **kwargs):
+    def auto_subscribe_user_to_what_he_comments(sender, **kwargs):
         comment = kwargs.pop('comment')
-        user = comment.user
-        Subscription.objects.subscribe(user, comment)
-    comment_was_posted.connect(auto_subscribe_user_to_his_comments)
-    
+        Subscription.objects.subscribe(comment.user, comment.content_object)
+        emit_new_comment(comment)
+    comment_was_posted.connect(auto_subscribe_user_to_what_he_comments)
+
     def emit_new_comment(comment):
         Subscription.objects.emit(
             u'%(actor)s commented on %(target)s',
@@ -36,18 +36,24 @@ if getattr(settings, 'USE_COMMENTS_AS_WALL', False):
         Subscription.objects.subscribe(user, user)
     signals.post_save.connect(auto_subscribe_user_to_himself, sender=User)
 
-    def auto_subscribe_user_to_what_he_comments(sender, **kwargs):
+    def auto_subscribe_user_to_his_comments(sender, **kwargs):
         comment = kwargs.pop('comment')
-        Subscription.objects.subscribe(comment.user, comment.content_object)
-        emit_new_comment(comment)
-    comment_was_posted.connect(auto_subscribe_user_to_what_he_comments)
+        user = comment.user
+        Subscription.objects.subscribe(user, comment)
+    comment_was_posted.connect(auto_subscribe_user_to_his_comments)
 
 if 'actstream' in APPS:
-    from actstream.models import Follow
+    from actstream.models import Follow, Action
 
     def auto_follow_notification(sender, **kwargs):
         emit_new_follower(kwargs['instance'].actor, kwargs['instance'].user)
     signals.post_save.connect(auto_follow_notification, sender=Follow)
+
+    def auto_subscribe_user_to_his_actions(sender, **kwargs):
+        action = kwargs['instance']
+        if action.actor.__class__.__name__ == 'User':
+            Subscription.objects.subscribe(action.actor, action)
+    signals.post_save.connect(auto_subscribe_user_to_his_actions, sender=Action)
 
     def emit_new_follower(user, follower):
         Subscription.objects.emit(
@@ -68,8 +74,11 @@ if 'django_messages' in APPS:
 
     def emit_new_message(message):
         Subscription.objects.emit(
-            u'%(actor)s sent a private message',
+            u'%(actor)s sent you a %(target)s',
             send_only_to=[message.recipient],
+            context={
+                'message': message,
+            },
             actor=message.sender,
             queue='chat',
         )
