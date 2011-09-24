bc797f56ee21a0c1c7d67a7e71a1a8e10b1b94da
James Pic <jamespic@gmail.com>
2011-09-25 00:18:20 +0200

    Capitalize rendered string in TextNotification, lowercase 'you'

--- subscription/examples/yourlabs/notifications/text.py
+++ subscription/examples/yourlabs/notifications/text.py
@@ -9,12 +9,12 @@ class TextNotification(base.BaseNotification):
 
         for key, value in self.format_kwargs.items():
             if user == value:
-                self.format_kwargs['%s_display' % key] = u'You'
+                self.format_kwargs['%s_display' % key] = u'you'
             elif hasattr(value, 'get_absolute_url'):
                 self.format_kwargs['%s_display' % key] = u'<a href="%s">%s</a>' % (
                     value.get_absolute_url(), unicode(value))
 
-        data['text'] = self.text % self.format_kwargs
+        data['text'] = self.text.capitalize() % self.format_kwargs
         return data
 
     def get_display(self, user, backend):
