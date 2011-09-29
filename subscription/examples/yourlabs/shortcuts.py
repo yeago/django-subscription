665e02552232d1834a3c7d2a3ada54c783633542
James Pic <jamespic@gmail.com>
2011-09-29 11:44:56 +0200

    Make the first argument of shortcuts \(notif class\) optionnal

--- subscription/examples/yourlabs/shortcuts.py
+++ subscription/examples/yourlabs/shortcuts.py
@@ -1,9 +1,14 @@
 import datetime
 
-from subscription.examples.yourlabs.notification import Lazy
+from subscription.examples.yourlabs.notification import Lazy, Notification
 
-def emit_lazy(cls, **kwargs):
+def emit_lazy(*args, **kwargs):
     kwargs['lazy'] = True
+
+    if len(args) == 0:
+        cls = Notification
+    else:
+        cls = args[0]
 
     if getattr(cls, 'kwargs_factory', False):
         kwargs = cls.kwargs_factory(**kwargs)
@@ -11,14 +16,25 @@ def emit_lazy(cls, **kwargs):
     for key, value in kwargs.items():
         kwargs[key] = Lazy(value)
 
+    if 'timestamp' not in kwargs.keys() and 'sent_at' not in kwargs.keys():
+        kwargs['sent_at'] = datetime.datetime.now()
+
     notification = cls(**kwargs)
 
     notification.emit()
     return notification
 
-def emit_static(cls, **kwargs):
+def emit_static(*args, **kwargs):
     kwargs['lazy'] = False
-    
+ 
+    if len(args) == 0:
+        cls = Notification
+    else:
+        cls = args[0]
+
+    if 'timestamp' not in kwargs.keys() and 'sent_at' not in kwargs.keys():
+        kwargs['sent_at'] = datetime.datetime.now()
+
     if getattr(cls, 'kwargs_factory', False):
         kwargs = cls.kwargs_factory(**kwargs)
     
