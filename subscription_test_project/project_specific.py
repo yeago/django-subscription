dd8391033f1d73a2df2307465535d298b4f50e3b
jpic <jamespic@gmail.com>
2011-12-22 22:01:54 +0100

    Fixed some recent BC breaks in test project

--- subscription_test_project/project_specific.py
+++ subscription_test_project/project_specific.py
@@ -4,27 +4,28 @@ from django.contrib.auth.models import User
 from django.contrib.comments.signals import comment_was_posted
 
 from subscription.models import Subscription
-from subscription.examples.yourlabs import notifications
 
 # deploy examples for a quick try
 from subscription.examples.yourlabs.apps import comments
 # auto subscribe users to objects they comment
-comments.comment_was_posted.connect(comments.comments_subscription)
+comments.signals.comment_was_posted.connect(comments.comments_subscription)
 
 # now, four DEAD SIMPLE examples to choose from:
 #
-comments.comment_was_posted.connect(
-    comments.comment_static_text_notification)
-#comments.comment_was_posted.connect(
+#comments.signals.comment_was_posted.connect(
+#    comments.comment_static_text_notification)
+#comments.signals.comment_was_posted.connect(
     #comments.comment_static_template_notification)
-#comments.comment_was_posted.connect(
+#comments.signals.comment_was_posted.connect(
     #comments.comment_lazy_text_notification)
-#comments.comment_was_posted.connect(
-    #comments.comment_lazy_template_notification)
+
+# best looking one :)
+comments.signals.comment_was_posted.connect(
+    comments.comment_lazy_template_notification)
 
 from subscription.examples.yourlabs.apps import auth
-auth.signals.post_save.connect(auth.auto_subscribe_user_to_himself, sender=User)
-auth.subscribe_existing_users_to_themselves()
+auth.signals.post_save.connect(auth.subscribe_user_to_himself, sender=User)
+auth.subscribe_existing_users_to_themselves(None)
 
 def user_detail(request, username,
     template_name='auth/user_detail.html', extra_context=None):
