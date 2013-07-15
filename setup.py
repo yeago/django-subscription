feec964efd818c455d337bb50dbda16279440ab1
yeago <subsume@gmail.com>
2013-07-15 05:02:44 +0200

    uptick version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-subscription',
-    version="0.0",
+    version="0.1",
     author='Steve Yeago',
     author_email='subsume@gmail.com',
     description='Managing subscriptions in Django',
