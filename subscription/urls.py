aa8c09c6b17e0b79c7a076811ea9f8b1ab158343
yeago <subsume@gmail.com>
2014-02-01 09:40:37 +0100

    extra space

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,7 +3,7 @@ from subscription.views import SubscriptionView
 
 urlpatterns = patterns('subscription.views',
    url(r'^$', SubscriptionView.as_view(), name="subscriptions"),
-    url(r'^actstream/ping/$', 'actstream_ping', name="actstream_ping"),
+   url(r'^actstream/ping/$', 'actstream_ping', name="actstream_ping"),
    url('unsubscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', 'unsubscribe', name="subscription_unsubscribe"),
    url('subscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', 'subscribe', name="subscription_subscribe"),
 )
