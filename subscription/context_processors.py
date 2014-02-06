9233c8a6cebc2c7af734dda54fed37898d86d907
yeago <subsume@gmail.com>
2014-02-07 00:37:26 +0100

    x

--- subscription/context_processors.py
+++ subscription/context_processors.py
@@ -8,6 +8,7 @@ def get_actstream(request):
     If we have 'undelivered' items, we deliver them to the unacknowledged list
     """
     stream = user_stream(request.user)
+    unacknowledged = None
     if stream:
         unacknowledged = request.user.get_profile(
             ).stream_pending_acknowledgements(stream[0][0])
