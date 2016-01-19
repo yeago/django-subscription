4480c0620ba269fca3e39caec66c110e24f847fd
steve yeago <subsume@gmail.com>
2016-01-19 11:38:11 -0800

    Update client.py

--- subscription/client.py
+++ subscription/client.py
@@ -14,9 +14,9 @@ class Redis(RedisBase):
             kwargs['host'] = settings.REDIS_HOST
         if not 'db' in kwargs:
             kwargs['db'] = settings.REDIS_DB
-        if getattr(settings, 'REDIS_PASSWORD' None):
+        if getattr(settings, 'REDIS_PASSWORD', None):
             kwargs['password'] = settings.REDIS_PASSWORD
-        super(Redis,self).__init__(**kwargs)
+        super(Redis, self).__init__(**kwargs)
 
 def get_cache_client(**kwargs):
     return Redis(**kwargs)
