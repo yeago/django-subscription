d0db866b65fef82e297cf164c5d5629247504526
steve yeago <subsume@gmail.com>
2020-08-14 14:06:13 -0400

    Update views.py

--- subscription/views.py
+++ subscription/views.py
@@ -20,21 +20,16 @@ def notifications_ping(request):  # Move unacknowledged items to acknowledged
     return HttpResponse("")
 
 
+@login_required
 def subscribe(request, content_type, object_id, success_message="Subscription added"):
     content_type = get_object_or_404(ContentType, pk=content_type)
-    if not request.user.is_authenticated():
-        raise Http404
-
     Subscription.objects.get_or_create(content_type=content_type, object_id=object_id, user=request.user)
     messages.success(request, success_message)
     return redirect(request.GET.get('return_url', '/'))
 
-
+@login_required
 def unsubscribe(request, content_type, object_id, success_message="You have been unsubscribed"):
     content_type = get_object_or_404(ContentType, pk=content_type)
-    if not request.user.is_authenticated():
-        raise Http404
-
     subscription = get_object_or_404(Subscription, content_type=content_type, object_id=object_id, user=request.user)
     subscription.delete()
     messages.success(request, success_message)
