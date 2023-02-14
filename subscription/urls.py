250881f1f97126488d0672e2610c4e9f8e4f3dbc
ivaggione <ivaggione@gmail.com>
2023-02-14 23:26:44 +0000

    Django 4

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from subscription import views
 
 urlpatterns = [
-   url(r'^$', views.SubscriptionView.as_view(), name="subscriptions"),
-   url(r'^notifications/ping/$', views.notifications_ping, name="notifications_ping"),
-   url('unsubscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', views.unsubscribe, name="subscription_unsubscribe"),
-   url('subscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', views.subscribe, name="subscription_subscribe"),
+   re_path(r'^$', views.SubscriptionView.as_view(), name="subscriptions"),
+   re_path(r'^notifications/ping/$', views.notifications_ping, name="notifications_ping"),
+   re_path('unsubscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', views.unsubscribe, name="subscription_unsubscribe"),
+   re_path('subscribe/(?P<content_type>\d+)/(?P<object_id>\d+)/', views.subscribe, name="subscription_subscribe"),
 ]
